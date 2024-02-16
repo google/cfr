1acc1ca63de798526e80b4fad23b4fb765a6fd94
Jason McCollum <jason.mccollum@woolpert.com>
2024-02-16 08:31:34 -0800

    Clamp current time to global duration

--- application/frontend/src/app/core/reducers/travel-simulator.reducer.ts
+++ application/frontend/src/app/core/reducers/travel-simulator.reducer.ts
@@ -8,7 +8,7 @@
  */
 import { createReducer, on } from '@ngrx/store';
 import Long from 'long';
-import { TravelSimulatorActions } from '../actions';
+import { ShipmentModelActions, TravelSimulatorActions } from '../actions';
 
 export const travelSimulatorKey = 'travelSimulator';
 
@@ -22,5 +22,30 @@ export const initialState: State = {
 
 export const reducer = createReducer(
   initialState,
-  on(TravelSimulatorActions.setTime, (state, { time }) => ({ ...state, time }))
+  on(TravelSimulatorActions.setTime, (state, { time }) => ({ ...state, time })),
+  // Keep time selection within the global range
+  on(ShipmentModelActions.setGlobalStartTime, (state, { globalStartTime }) => ({
+    ...state,
+    time: Math.max(Long.fromValue(globalStartTime).toNumber(), state.time),
+  })),
+  on(ShipmentModelActions.setGlobalEndTime, (state, { globalEndTime }) => ({
+    ...state,
+    time: Math.min(Long.fromValue(globalEndTime).toNumber(), state.time),
+  })),
+  on(ShipmentModelActions.setShipmentModel, (state, newState) => {
+    let newTime = state.time;
+
+    if (newState.globalStartTime) {
+      newTime = Math.max(Long.fromValue(newState.globalStartTime).toNumber(), newTime);
+    }
+
+    if (newState.globalEndTime) {
+      newTime = Math.min(Long.fromValue(newState.globalEndTime).toNumber(), newTime);
+    }
+
+    return {
+      ...state,
+      time: newTime,
+    };
+  })
 );
